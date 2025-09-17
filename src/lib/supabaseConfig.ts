baff62a4d601d1683c921c4acd7a2c8c767f6bff
febx69 <86050097+febx69@users.noreply.github.com>
2025-09-17 08:20:41 +0700

    Update supabaseConfig.ts

--- src/lib/supabaseConfig.ts
+++ src/lib/supabaseConfig.ts
@@ -3,5 +3,5 @@
  * Ganti nilai di bawah ini dengan URL dan ANON KEY project Supabase Anda.
  * ANON KEY bersifat public dan aman berada di frontend.
  */
-export const SUPABASE_URL = 'https://gkhcjbmmlofbmtkxgnca.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdraGNqYm1tbG9mYm10a3hnbmNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgwNzAzODIsImV4cCI6MjA3MzY0NjM4Mn0.TYqrCblem23SbO1oxU8SUdsszWRd_cw1Njzf0jdSu8I';
+export const SUPABASE_URL = '';
+export const SUPABASE_ANON_KEY = '';
